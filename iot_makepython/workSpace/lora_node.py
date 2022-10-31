0c925b18681821ef6eee5cf08284aec5a59e7f51
Christof Schmid <christof.schmid@aaa-immobilien.ch>
2022-10-31 18:17:56 +0100

    Adjust frequency to 433

--- iot_makepython/workSpace/lora_node.py
+++ iot_makepython/workSpace/lora_node.py
@@ -4,11 +4,12 @@ import time
 import config_lora
 import random
 import webserver
+import ntptime
 try:
   import usocket as socket
 except:
   import socket
-import ntptime
+
 
  
 class Lora_Gate:
@@ -22,6 +23,8 @@ class Lora_Gate:
         self.buff = ""
         self.ip = ip
         self.node_list = Lora_Node_List()
+        self.DisplayTimerFlag = 0
+        
 
     #模式配置
     def working(self):
@@ -59,12 +62,18 @@ class Lora_Gate:
 
         while True:
             now_time = time.time()
+            disp_time = time.time()
+            
+            #if now_time - disp_time > 30 and self.DisplayTimerFlag == 1:
+              #self.show_all_status(self.node_list)
+              #continue
+            
             if now_time - last_time > 60 or relay_common == 1:
                 last_time = now_time
                 relay_common = 0
 
                 tm = time.localtime()
-                t_hour = (tm[3] + 8) % 24
+                t_hour = (tm[3] + 2) % 24
                 t_min = tm[4]
                 t_sec = tm[5]
                 time_str = str(t_hour) + ':' + str(t_min) + ':' + str(t_sec)
@@ -74,9 +83,10 @@ class Lora_Gate:
                 self.sendMessage("PREPARE")
                 time.sleep(5)
 
-                last_node_status = {"Relay":"NULL", "Soil1":"NULL", "Soil2": "NULL", "Soil3":"NULL", "Update_time":"NULL"}
+                last_node_status = {"Relay":"NULL", "Soil1":"NULL", "Soil2": "NULL", "Soil3":"NULL", "Soil4":"NULL", "Soil5":"NULL", "Soil6":"NULL", "Update_time":"NULL"}
                 last_node_status["Update_time"] = time_str
-
+                
+                self.DisplayTimerFlag = 1
                 temp_msg = "RELAY" + onoff
                 onoff = ""
                 self.sendMessage(temp_msg)
@@ -88,7 +98,7 @@ class Lora_Gate:
                 if last_node_status["Relay"] == "":
                     last_node_status["Relay"] = "NULL"
 
-                for i in range(1,4):
+                for i in range(1,7):
                     soil_index = 'Soil'+str(i)
                     self.sendMessage(soil_index)
                     self.lora.receive()
@@ -177,39 +187,91 @@ class Lora_Gate:
                 print("Callback time out.")
                 break
         self.flag = 0
+        
+    def lora_timeout1(self):
+        print("Display time out")
+        now = config_lora.millisecond()
+        while(1):
+            if config_lora.millisecond() - now > 3000:
+                print("Changing display")
+                break
+        
 
     def show_all_status(self,node_List):
         self.lora.show_text(self.ip)
-
+        
         soil0 = node_List.list[9]['Soil1']
-        if soil0 == "AHT10ERR" :
+        if soil0 == "AHT10ERR" : 
             pass
         elif soil0 != "NULL" :
-            soil0 = soil0[soil0.find('ADC'):]
+            soil0 = soil0[soil0.find('H:'):]
+            soil0 = soil0 + soil0[soil0.find('B:'):]
 
 
         soil1 = node_List.list[9]['Soil2']
         if soil1 == "AHT10ERR" :
             pass
         elif soil1 != "NULL" :
-            soil1 = soil1[soil1.find('ADC'):]
+            soil1 = soil1[soil1.find('H:'):]
+            soil1 = soil1 + soil1[soil1.find('B:'):]
 
 
         soil2 = node_List.list[9]['Soil3']
         if soil2 == "AHT10ERR" :
             pass
         elif soil2 != "NULL" :
-            soil2 = soil2[soil2.find('ADC'):]
-
-        self.lora.show_text('Relay:'+ node_List.list[9]['Relay'],0,10,clear_first = False)
-        self.lora.show_text('Soil1:'+ soil0,0,20,clear_first = False)
-        self.lora.show_text('Soil2:'+ soil1,0,30,clear_first = False)
-        self.lora.show_text('Soil3:'+ soil2,0,40,clear_first = False)
-        self.lora.show_text(node_List.list[9]['Update_time'],0,50,clear_first = False, show_now = True, hold_seconds = 1)
+            soil2 = soil2[soil2.find('H:'):]
+            soil2 = soil2 + soil2[soil2.find('B:'):]
+            
+        soil3 = node_List.list[9]['Soil4']
+        if soil3 == "AHT10ERR" :
+            pass
+        elif soil3 != "NULL" :
+            soil3 = soil3[soil3.find('H:'):]
+            soil3 = soil3 + soil3[soil3.find('B:'):]
+            
+        soil4 = node_List.list[9]['Soil5']
+        if soil4 == "AHT10ERR" :
+            pass
+        elif soil4 != "NULL" :
+            soil4 = soil4[soil4.find('H:'):]
+            soil4 = soil4 + soil4[soil4.find('B:'):]
+            
+            
+        soil5 = node_List.list[9]['Soil6']
+        if soil5 == "AHT10ERR" :
+            pass
+        elif soil5 != "NULL" :
+            soil5 = soil5[soil5.find('H:'):]
+            soil5 = soil5 + soil5[soil5.find('B:'):]
+            
+        
+        
+        while self.DisplayTimerFlag == 1:
+          self.lora.show_text(self.ip,0,0,clear_first = True)
+          self.lora.show_text('R1:'+ node_List.list[9]['Relay'],0,10,clear_first = False)
+          self.lora.show_text('S1:'+ soil0,0,20,clear_first = False)
+          self.lora.show_text('S2:'+ soil1,0,30,clear_first = False)
+          self.lora.show_text('S3:'+ soil2,0,40,clear_first = False)
+          self.lora.show_text(node_List.list[9]['Update_time'],0,50,clear_first = False, show_now = True, hold_seconds = 1)
+        
+          self.lora_timeout1()
+          
+          self.lora.show_text(self.ip,0,0,clear_first = True)
+          self.lora.show_text('R1:'+ node_List.list[9]['Relay'],0,10,clear_first = False)
+          self.lora.show_text('S4:'+ soil3,0,20,clear_first = False)
+          self.lora.show_text('S5:'+ soil4,0,30,clear_first = False)
+          self.lora.show_text('S6:'+ soil5,0,40,clear_first = False) 
+          self.lora.show_text(node_List.list[9]['Update_time'],0,50,clear_first = False, show_now = True, hold_seconds = 1)         
+          #self.DisplayTimerFlag = 0
+          
+          
+              
 
 class Lora_Node_List:
     def __init__(self):
         self.list = []
         for i in range(10):
-            self.list.append({"Relay":"default", "Soil1":"default", "Soil2": "default", "Soil3":"default", "Update_time":"00:00:00"})
+            self.list.append({"Relay":"default", "Soil1":"default", "Soil2":"default", "Soil3":"default", "Soil4":"default", "Soil5":"default", "Soil6":"default","Update_time":"00:00:00"})
+
 
